3c5ae6020122eb11146ea3ba27c34d293e911392
David G <david.grueso@correounivalle.edu.co>
2025-05-07 20:19:14 -0500

    Desarrollo de la clase Avatar con su implementacion

--- Avatar.cpp
+++ Avatar.cpp
@@ -1,13 +1,71 @@
 #include "Avatar.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
 
 
+Avatar::Avatar(string name, Tablero& tableroReferencia, int posFila, int posColumna) : name(name), tableroReferencia(tableroReferencia), posFila(0), posColumna(0) {}
 
-Avatar::Avatar(string name)
+
+void Avatar::iniciarPosicionRandom()
 {
-	this->name = name;
+	this->posFila = rand() % 10; //Fila aleatoria entre 0 y 9
+	this->posColumna = rand() % 10; //Columna aleatoria entre 0 y 9
 }
 
+bool Avatar::verificarPosicion()
+{	
+	//verificar que la posicion este dentro de los limites del tablero
+	//minimo y maximo son los limites del tablero, las constantes que definimos en la clase Avatar
+	if ((posFila >= minimo && posFila < maximo) && (posColumna >= minimo && posColumna < maximo)) 
+	{
+		//verificar que la posicion sea un 1 en el tablero.
+		if(tableroReferencia.getMatrizTablero()[posFila][posColumna] == 1)
+		{
+			return true;
+		}
+	}
+	else { return false; } 
 
-Avatar::~Avatar()
-{
 }
+
+bool Avatar::verificarSalida()
+{
+	if(tableroReferencia.getSalidaFila() == posFila && tableroReferencia.getSalidaColumna() == posColumna) //verificamos si la posicion es la salida
+	{
+		return true; //El avatar ha encontrado la salida
+	}
+	else{ return false; } //El avatar no ha encontrado la salida
+}
+
+void Avatar::moverPosicion(int &posicionNueva, int cambio)
+{
+	//verificar que la posicion este dentro de los limites del tablero
+	if((posicionNueva + cambio >= minimo) && (posicionNueva + cambio < maximo)) //verificamos que la nueva posicion este dentro de los limites del tablero
+	{
+		posicionNueva += cambio; //movemos el avatar a la nueva posicion
+	}	
+}
+
+void Avatar::moverArriba()
+{
+	moverPosicion(posFila, -1); //Se le resta 1 a la fila para mover el avatar hacia arriba
+}
+
+void Avatar::moverAbajo()
+{
+	moverPosicion(posFila, 1); //Se le suma 1 a la fila para mover el avatar hacia abajo
+}
+
+void Avatar::moverIzquierda()
+{
+	moverPosicion(posColumna, -1); //Se le resta 1 a la columna para mover el avatar hacia la izquierda
+}
+
+void Avatar::moverDerecha()
+{
+	moverPosicion(posColumna, 1); //Se le suma 1 a la columna para mover el avatar hacia la derecha
+}
+
--- Avatar.h
+++ Avatar.h
@@ -1,20 +1,48 @@
 #ifndef AVATAR_H
 #define AVATAR_H
 #include <string>
+#include "Tablero.h"
 using namespace std;	
 
 class Avatar {
 	
 	private:
+	
+		//Atributos
 		string name;
+		//Posicion del avatar en el tablero
+		int posFila;
+		int posColumna;
+		//La clase avatar tiene una referencia a la clase Tablero para verificar la posicion del avatar
+		Tablero& tableroReferencia;
 		
-	
-
+		//Limites del tablero. Lo usamos para el movimiento del avatar
+		static const int minimo = 0; //Limite inferior del tablero (0)
+		static const int maximo = 10; //Tamaño del tablero (10x10)
+		
 	public:
-	
-		Avatar(string name);
-		~Avatar();
+
+		//Constructor 
+		Avatar(string name, Tablero& tableroReferencia, int posFila, int posColumna);
+
+		//Funciones iniciales y de verificacion
+		void iniciarPosicionRandom();
+		bool verificarPosicion(); 
+		bool verificarSalida(); //verifica si el avatar ha encontrado la salida
 		
+		//Movimiento del avatar
+		//El parametro posicionNueva es la posicion a modificar, por eso se pasa por referencia
+		//El parametro cambio es el valor a sumar o restar a la posicion
+		void moverPosicion(int& posicionNueva, int cambio); //mueve el avatar a una nueva posicion
+		void moverArriba();
+		void moverAbajo();
+		void moverIzquierda();
+		void moverDerecha();
+		
+		//Getters 
+		string getName() {return this->name;}; 
+		int getPosFila() {return this->posFila;};
+		int getPosColumna() {return this->posColumna;}; 
 
 		
 };
