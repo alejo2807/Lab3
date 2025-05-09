dab42a5f0d33bc9c65c1842b0e8d85f0dff812ed
David G <david.grueso@correounivalle.edu.co>
2025-05-09 18:07:10 -0500

    Corregir main para su correcto funcionamiento

--- main.cpp
+++ main.cpp
@@ -1,55 +1,15 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
 #include "Avatar.h"
 #include "Tablero.h"
-#include <string>
-
-// g++ main.cpp Tablero.cpp -o p
+#include "Controlador.h"
+// g++ main.cpp Tablero.cpp ClaseVista.cpp Controlador.cpp Avatar.cpp -o p
 int main()
-{
+{	
+	Tablero tableroMain; //Creamos el tablero
+	Avatar avatarMain("Avatar", tableroMain, 0, 0); //Creamos el avatar
+	ClaseVista vistaMain; //Creamos la vista
 	
-	//Una semilla es un número que se utiliza para inicializar un generador de números aleatorios.
-	//La función srand() se utiliza para establecer la semilla del generador de números aleatorios.
-	//La funcion srand() genera una secuencia de números pseudoaleatorios.
-	//Esto significa que cada vez que se ejecute el programa, se generarán diferentes secuencias de números "aleatorios".
-	//La función time(0) devuelve el tiempo actual en segundos desde el 1 de enero de 1970. Y este numero se lo pasamos como parametro  
-	//a srand() ya que al cambiar cada segundo, la semilla que iniciamos siempre cambia, lo cual hace tableros diferentes (secuencias diferentes)
-	srand(time(0)); 
-	
-	//creacion del tablero del juego
-	Tablero tablero;  
-	tablero.caminoPrincipal(); // Definimos el camino principal del tablero
-	tablero.rellenarTableroCompleto(); // Llenamos el tablero con números aleatorios (0s y 1s)
-	//tablero.mostrarTablero(); // Mostramos el tablero en pantalla
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	//Creamos el controlador, pasandole el tablero, el avatar y la vista
+	Controlador controlador(tableroMain, avatarMain, vistaMain); //Creamos el controlador
+	controlador.iniciarJuego(); //Iniciamos el juego
 	return 0;
 }
-
-
-
-
-/*
-int main() {
-
-
-	srand(time(0)); //semilla del tiempo
-
-	std::cout << "3 números generados con la semilla del tiempo " << ":\n";
-	std::cout << rand() << std::endl;
-	std::cout << rand() << std::endl;
-	std::cout << rand() << std::endl;
-
-	return 0;
-}
-
-*/
